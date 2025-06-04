d715dbfd3c41495815eef3493e2c3512edea47c8
surajyadav887 <ksuraj8873@gmail.com>
2025-06-04 11:56:42 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Component/Navbar/Navbar'
 import Herosection from './Component/Herosection/Herosection'
+import Footer from './Component/Footer/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Herosection/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h1>suraj yadav lauda</h1>
+    </div>
+  )
+}
+
+export default Footer
